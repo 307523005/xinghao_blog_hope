ebb57ad4b83ff4e0ea1f830aa1d7af31b143b293
兴浩 <niuhao.nh@digital-engine.com>
2023-04-12 14:00:15 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,10 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
+git push -f git@github.com:307523005/xinghao_blog_hope.git master:pages
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:307523005/xinghao_blog_hope.git master:pages
 cd -
